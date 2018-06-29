350156d1dd13c30136c1aa1c7ac574c3ac0622d6
Yangkai.Shen <237497819@qq.com>
2018-06-29 16:50:38 +0800

    :tada: DailyLog project has been started. This is the first initial commit.

---
+++ src/main/java/com/xkcoding/dailylog/DailylogApplication.java
@@ -0,0 +1,25 @@
+package com.xkcoding.dailylog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <p>
+ * 程序启动类
+ * </p>
+ *
+ * @package: com.xkcoding.dailylog
+ * @description： 程序启动类
+ * @author: yangkai.shen
+ * @date: Created in 2018/6/29 下午4:47
+ * @copyright: Copyright (c) 2018
+ * @version: V1.0
+ * @modified: yangkai.shen
+ */
+@SpringBootApplication
+public class DailyLogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyLogApplication.class, args);
+	}
+}
